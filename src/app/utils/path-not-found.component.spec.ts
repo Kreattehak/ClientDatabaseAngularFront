7cb4e07361309cda2d98b6d4fa6129c1ba02ee3f
Kreattehak <nerokamil@gmail.com>
2017-12-18 15:50:19 +0100

    First component tests

---
+++ src/app/utils/path-not-found.component.spec.ts
@@ -0,0 +1,27 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {PathNotFoundComponent} from './path-not-found.component';
+
+describe('PathNotFoundComponent', () => {
+  let component: PathNotFoundComponent;
+  let fixture: ComponentFixture<PathNotFoundComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        PathNotFoundComponent
+      ],
+    });
+    fixture = TestBed.createComponent(PathNotFoundComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the PathNotFoundComponent', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have properly created template', () => {
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('p').textContent).toContain('Path not found!');
+  });
+});
