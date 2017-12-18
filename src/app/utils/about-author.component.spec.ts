afdef19277fe7ede1dc512ebe96e3440165a8ec4
Kreattehak <nerokamil@gmail.com>
2017-12-18 15:51:14 +0100

    Add AboutAuthorComponent tests

--- src/app/utils/about-author.component.spec.ts
+++ src/app/utils/about-author.component.spec.ts
@@ -1,19 +1,32 @@
-import {TestBed, async} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AboutAuthorComponent} from './about-author.component';
 
 describe('AboutAuthorComponent', () => {
-  beforeEach(async(() => {
+  let component: AboutAuthorComponent;
+  let fixture: ComponentFixture<AboutAuthorComponent>;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         AboutAuthorComponent
       ],
-    }).compileComponents();
-  }));
+    });
+    fixture = TestBed.createComponent(AboutAuthorComponent);
+    component = fixture.componentInstance;
+  });
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AboutAuthorComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  it('should create the AboutAuthorComponent', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have properly created template', () => {
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('h1').textContent).toContain('Hello,');
+    expect(compiled.querySelector('h2').textContent).toContain('secondary route');
+    expect(compiled.querySelector('p').textContent).toContain('other projects');
+    expect(compiled.querySelector('p').nextElementSibling.textContent).toContain('Kreattehak');
+    expect(compiled.querySelector('a').href).toContain('https://github.com/Kreattehak');
+  });
 });
