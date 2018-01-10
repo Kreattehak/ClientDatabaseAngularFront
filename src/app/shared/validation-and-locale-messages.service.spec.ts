793f213588645a830ccd65449d3bc3f78a33bf63
Kreattehak <nerokamil@gmail.com>
2018-01-10 12:01:33 +0100

    Add tests for ValidationAndLocaleMessagesService

---
+++ src/app/shared/validation-and-locale-messages.service.spec.ts
@@ -0,0 +1,75 @@
+import {ValidationAndLocaleMessagesService} from './validation-and-locale-messages.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+const form = new FormGroup({
+  id: new FormControl(''),
+  firstName: new FormControl('', [Validators.required, Validators.minLength(3)])
+});
+const formErrors = { firstName: '' };
+
+describe('ValidationAndLocaleMessagesService', () => {
+
+  let service: ValidationAndLocaleMessagesService;
+
+  beforeEach(() => {
+    service = new ValidationAndLocaleMessagesService('en');
+  });
+
+  it('should add cookie to document', () => {
+    service.setCookie();
+
+    expect(document.cookie).toBe('myLocaleCookie=en');
+  });
+
+  it('should get english validation message', () => {
+    const validationMessage = service.getLocalizedValidationMessages('firstName');
+
+    expect(validationMessage.required).toContain('First name');
+  });
+
+  it('should get polish validation message', () => {
+    service.localeId = 'pl';
+
+    const validationMessage = service.getLocalizedValidationMessages('firstName');
+
+    expect(validationMessage.required).toContain('Imię');
+  });
+
+  it('should get english message', () => {
+    const validationMessage = service.getLocalizedMessages('errorTitle');
+
+    expect(validationMessage).toContain('Error');
+  });
+
+  it('should get polish message', () => {
+    service.localeId = 'pl';
+
+    const validationMessage = service.getLocalizedMessages('errorTitle');
+
+    expect(validationMessage).toContain('Błąd');
+  });
+
+  it('should not validate form and return early when form is null', () => {
+    setUpErrorOnControl();
+    service.onValueChanged(null, formErrors);
+
+    expect(formErrors.firstName).not.toContain('is required');
+  });
+
+  it('should validate form', () => {
+    setUpErrorOnControl();
+
+    service.onValueChanged(form, formErrors);
+
+    expect(formErrors.firstName).toContain('is required');
+  });
+
+  function setUpErrorOnControl() {
+    const control = form.get('firstName');
+    control.markAsDirty();
+    control.markAsTouched();
+    control.setErrors({
+      required: true
+    });
+  }
+});
