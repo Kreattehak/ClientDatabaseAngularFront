e101e6ef6caa323d0e227bb2bd17620ccaab9fe6
Kreattehak <nerokamil@gmail.com>
2018-01-10 12:03:54 +0100

    Update AddressServiceStub logic with server errors

--- src/test/address.service.stub.ts
+++ src/test/address.service.stub.ts
@@ -4,11 +4,25 @@ import {Address} from '../app/addresses/address';
 
 @Injectable()
 export class AddressServiceStub {
+  public errorOccurred: boolean;
+  public errorResponseOccurred: boolean;
+
   saveNewAddress(newAddress: Address, clientId: number): Observable<number> {
-    return Observable.of(1);
+    if (this.errorResponseOccurred) {
+      return Observable.throw('Something went wrong!');
+    }
+    if (this.errorOccurred) {
+      return Observable.throw(-1);
+    } else {
+      return Observable.of(1);
+    }
   }
 
   updateAddress(updatedAddress: Address): Observable<string> {
-    return Observable.of('Everything went fine!');
+    if (this.errorOccurred) {
+      return Observable.throw('Something went wrong!');
+    } else {
+      return Observable.of('Everything went fine!');
+    }
   }
 }
