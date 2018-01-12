ad9ff1533a3b156449f8dfc58d396954607d0ae4
Kreattehak <nerokamil@gmail.com>
2018-01-12 13:33:55 +0100

    Add/update tests for AddressDetailComponent

--- src/app/addresses/address-detail.component.spec.ts
+++ src/app/addresses/address-detail.component.spec.ts
@@ -13,24 +13,26 @@ import {ValidationAndLocaleMessagesServiceStub} from '../../test/validation-and-
 import {AddressServiceStub} from '../../test/address.service.stub';
 import {ToastsManagerStub} from '../../test/toasts-manager.stub';
 import {Address} from './address';
+import {RouterStub} from '../../test/router.stub';
 
 const activatedRouteStub = new ActivatedRouteStub();
+const routerStub = new RouterStub();
 const validationServiceStub = new ValidationAndLocaleMessagesServiceStub();
 const addressServiceStub = new AddressServiceStub();
 const toastsManagerStub = new ToastsManagerStub();
 
-const addressData = {
+export const ADDRESS_DATA = {
   id: 1,
   streetName: 'street',
   cityName: 'city',
   zipCode: '44-444'
 };
-const clientData = {
+export const CLIENT_DATA = {
   id: 1,
   firstName: 'Fake',
   lastName: 'User',
   dateOfRegistration: '11-05-1974',
-  mainAddress: addressData
+  mainAddress: ADDRESS_DATA
 };
 
 describe('AddressDetailComponent', () => {
@@ -53,7 +55,8 @@ describe('AddressDetailComponent', () => {
         {provide: AddressService, useValue: addressServiceStub},
         {provide: ValidationAndLocaleMessagesService, useValue: validationServiceStub},
         {provide: ToastsManager, useValue: toastsManagerStub},
-        {provide: ActivatedRoute, useValue: activatedRouteStub}
+        {provide: ActivatedRoute, useValue: activatedRouteStub},
+        {provide: Router, useValue: routerStub}
       ]
     });
     fixture = TestBed.createComponent(AddressDetailComponent);
@@ -62,6 +65,8 @@ describe('AddressDetailComponent', () => {
 
   afterEach(() => {
     activatedRouteStub.resetData();
+    toastsManagerStub.message = '';
+    addressServiceStub.errorOccurred = false;
   });
 
   it('should create the app', () => {
@@ -81,26 +86,33 @@ describe('AddressDetailComponent', () => {
     ]);
   });
 
+  it('should subscribe to valueChanges and use ValidationService', () => {
+    const spy = spyOn(validationServiceStub, 'onValueChanged');
+    fixture.detectChanges();
+
+    expect(spy).toHaveBeenCalledWith(component.addressForm, component.formErrors);
+  });
+
   it('should populate form with fetched data', () => {
     activatedRouteStub.testParamMap = {id: 1, addressId: 1};
     activatedRouteStub.testData = {
-      addresses: new Array<Address>(addressData),
-      client: clientData
+      addresses: new Array<Address>(ADDRESS_DATA),
+      client: CLIENT_DATA
     };
     fixture.detectChanges();
 
-    expect(component.activeAddress).toBe(addressData);
-    expect(component.activeClient).toBe(clientData);
+    expect(component.activeAddress).toBe(ADDRESS_DATA);
+    expect(component.activeClient).toBe(CLIENT_DATA);
   });
 
   it('should create form for new address', () => {
     activatedRouteStub.testParamMap = {id: 1};
-    activatedRouteStub.testData = {client: clientData};
+    activatedRouteStub.testData = {client: CLIENT_DATA};
     fixture.detectChanges();
 
     expect(component.activeAddress).toBeTruthy();
     expect(component.activeAddress.id).toBe(undefined);
-    expect(component.activeClient).toBe(clientData);
+    expect(component.activeClient).toBe(CLIENT_DATA);
   });
 
   it('should route to given client details', inject([Router], (router: Router) => {
@@ -144,20 +156,29 @@ describe('AddressDetailComponent', () => {
     expect(spy).toHaveBeenCalledWith(activeAddress, Number(null));
   });
 
-  it('should try to update existing address', () => {
+  it('should show error when trying to add new address', () => {
     fixture.detectChanges();
-    component.isNewAddress = false;
-    setAnotherActiveAddress();
+    component.isNewAddress = true;
+    setActiveAddress();
+    addressServiceStub.errorOccurred = true;
 
-    const activeAddress = setAddressFormWithDuplicatedData();
-    const spy = spyOn(addressServiceStub, 'updateAddress').and.callThrough();
+    component.onSubmit(1);
 
-    component.onSubmit(1); // it's not clientId
-
-    expect(spy).toHaveBeenCalledWith(activeAddress);
+    expect(toastsManagerStub.message).toContain('fakeAnswer');
   });
 
-  it('should try to update existing address', () => {
+  it('should show error response when trying to add new address', () => {
+    fixture.detectChanges();
+    component.isNewAddress = true;
+    setActiveAddress();
+    addressServiceStub.errorResponseOccurred = true;
+
+    component.onSubmit(1);
+
+    expect(toastsManagerStub.message).toContain('wrong');
+  });
+
+  it('should update existing address', () => {
     fixture.detectChanges();
     component.isNewAddress = false;
     setAnotherActiveAddress();
@@ -165,27 +186,32 @@ describe('AddressDetailComponent', () => {
     const activeAddress = setAddressFormWithDuplicatedData();
     const spy = spyOn(addressServiceStub, 'updateAddress').and.callThrough();
 
-    component.onSubmit(999); // it's not clientId
+    component.onSubmit(999);
 
     activeAddress.id = 999;
     expect(spy).toHaveBeenCalledWith(activeAddress);
   });
 
-  it('should try to update existing address', () => {
-    activatedRouteStub.testParamMap = {'clientId': '1'};
+  it('should show error when trying to update existing address', () => {
     fixture.detectChanges();
-    console.log(component.activeClient);
+    component.isNewAddress = false;
+    setAnotherActiveAddress();
+    addressServiceStub.errorOccurred = true;
+
+    component.onSubmit(1);
+
+    expect(toastsManagerStub.message).toContain('wrong');
   });
 
   function setAddressFormWithDuplicatedData() {
-    const data = addressData;
+    const data = {...ADDRESS_DATA}; // prevent test model object to change
     component.addressForm.setValue(data);
 
     return data;
   }
 
   function setActiveAddress() {
-    component.activeAddress = addressData;
+    component.activeAddress = ADDRESS_DATA;
   }
 
   function setAnotherActiveAddress() {
