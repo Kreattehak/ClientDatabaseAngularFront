dfe5a702e6789bd89164e06081d6012e89136b21
Kreattehak <nerokamil@gmail.com>
2018-01-15 13:57:51 +0100

    Broadening spectrum of BootboxStub behaviors

--- src/test/bootbox.stub.ts
+++ src/test/bootbox.stub.ts
@@ -3,12 +3,19 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class BootboxStub {
   public message: string;
+  public isConfirmedAction = false;
 
   alert(data: any): void {
     this.message = data.message;
   }
 
+  confirm(data: any): void {
+    this.message = data.message;
+    data.callback(this.isConfirmedAction);
+  }
+
   resetData(): void {
     this.message = '';
+    this.isConfirmedAction = false;
   }
 }
